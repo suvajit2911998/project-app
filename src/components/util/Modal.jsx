45de5fc9dfb2cdf4d68fc104ef21cb43e7f446fe
Suvajit Sarkar <suvajit2911998@gamil.com>
2025-04-22 03:07:34 +0530

    Update: Fixed Modal Style

--- src/components/util/Modal.jsx
+++ src/components/util/Modal.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import '../ContactUs/ContactUsFrom.css';
+import './Modal.css';
 
 export default function Modal({ children }) {
     return ReactDOM.createPortal(
